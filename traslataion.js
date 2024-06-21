51e422f23679c6510259632ec9a5cc49f89a0522
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-06-21 18:48:38 -0500

    oka translation

--- traslataion.js
+++ traslataion.js
@@ -1,40 +1,30 @@
-// import { HfInference } from "@huggingface/inference";
-// import { config } from "dotenv";
+import { HfInference } from "@huggingface/inference";
+import { config } from "dotenv";
 
-// config(); // Carga las variables de entorno desde el archivo .env
+config(); // Carga las variables de entorno desde el archivo .env
 
-// const imageURL =
-// 	"https://somosmuyperros.com/wp-content/uploads/2017/02/Miniatura_comportamiento17-1200x1012.jpg";
-// const model = "Salesforce/blip-image-captioning-large";
+const model = "facebook/nllb-200-distilled-600M";
 
-// // Asegúrate de que el token esté cargado correctamente desde las variables de entorno
-// const inferencehf = new HfInference(process.env.HF_TOKEN);
+// Asegúrate de que el token esté cargado correctamente desde las variables de entorno
+const inferencehf = new HfInference(process.env.HF_TOKEN);
 
-// // Función asincrónica para ejecutar la inferencia
-// async function runInference() {
-// 	try {
-// 		const response = await fetch(imageURL);
-// 		const blob = await response.blob();
+// Función asincrónica para ejecutar la inferencia
+async function runInference() {
+	try {
+		const result = await inferencehf.translation({
+			model: model,
+			inputs: "Hello world",
+			parameters: {
+				src_lang: "en",
+				tgt_lang: "es",
+			},
+		});
 
-// 		const result = await inferencehf.imageToText({
-// 			data: blob,
-// 			model: model,
-// 		});
+		console.log(result);
+	} catch (error) {
+		console.error("Error during inference:", error);
+	}
+}
 
-// 		console.log(result);
-// 	} catch (error) {
-// 		console.error("Error during inference:", error);
-// 	}
-// }
-
-// // Ejecuta la función
-// runInference();
-
-// await hf.translation({
-//   model: 'facebook/mbart-large-50-many-to-many-mmt',
-//   inputs: textToTranslate,
-//   parameters: {
-//   "src_lang": "en_XX",
-//   "tgt_lang": "fr_XX"
-//  }
-// })
+// Ejecuta la función
+runInference();
